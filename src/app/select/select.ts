5015b1cdf072ef30d0f143164087a8652ac59499
ahmadsiahmansoori <ahmad.s96736@gmail.com>
2025-08-14 23:18:48 +0330

    component select init

---
+++ src/app/select/select.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+
+export interface SelectInterface {
+
+}
+
+
+@Component({
+  selector: 'app-select',
+  imports: [],
+  templateUrl: './select.html',
+  styleUrl: './select.css'
+})
+export class Select {
+
+
+
+
+}
