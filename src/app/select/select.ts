54f5571175331774d4a1c693e5d05baff87862c4
Ahmad Siahmansouri <a.siahmansouri@yarhis.com>
2025-08-16 12:44:17 +0330

    fixed select component issue

--- src/app/select/select.ts
+++ src/app/select/select.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, input, output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
-
+import {MatSelectModule} from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 export interface SelectInterface {
   label: string,
   bindLabel: string,
@@ -11,19 +14,39 @@ export interface SelectInterface {
 
 @Component({
   selector: 'app-select',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatFormFieldModule
+  ],
   templateUrl: './select.html',
   styleUrl: './select.css'
 })
-export class Select {
+export class Select<T = any>  implements OnInit{
 
 
-
-  @Input({required: true}) config!: SelectInterface
-  @Input({required: true}) options: any[] = [];
-  @Input({required: false}) value: any;
+  control = new FormControl();
 
 
+  config = input.required<SelectInterface>();
+  valueChange = output<T | T[] | null>();
+  options = input<any[]>([])
+
+
+  @Input()
+  set value(val: T | T[] | null) {
+    this.control.setValue(val);
+  }
+
+  select(item: any) {
+
+    this.valueChange.emit(item)
+  }
+
+  ngOnInit(): void {
+    this.control.valueChanges.subscribe(console.log)
+  }
 
 
 }
