32558eaed76e9e86b47c3ce2b775d1681f69e708
Ahmad Siahmansouri <a.siahmansouri@yarhis.com>
2025-09-03 22:24:07 +0330

    validators input append date input

--- src/app/date-picker/date-picker.ts
+++ src/app/date-picker/date-picker.ts
@@ -3,13 +3,23 @@ import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } f
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
 import { JalaliMomentDateAdapter } from '../JalaliMomentDateAdapter';
 
 import moment from 'jalali-moment';
 
-export const PERSIAN_DATE_FORMATS = { parse: { dateInput: 'YYYY/MM/DD' }, display: { dateInput: 'YYYY/MM/DD', monthYearLabel: 'YYYY MMM', dateA11yLabel: 'YYYY/MM/DD', monthYearA11yLabel: 'YYYY MMMM', }, };
+// export const PERSIAN_DATE_FORMATS = { parse: { dateInput: 'YYYY/MM/DD' }, display: { dateInput: 'YYYY/MM/DD', monthYearLabel: 'YYYY MMM', dateA11yLabel: 'YYYY/MM/DD', monthYearA11yLabel: 'YYYY MMMM', }, };
+
+export const PERSIAN_DATE_FORMATS = {
+  parse: { dateInput: 'jYYYY/jMM/jDD' },
+  display: {
+    dateInput: 'jYYYY/jMM/jDD',      // What shows inside <input>
+    monthYearLabel: 'jYYYY jMMMM',
+    dateA11yLabel: 'jYYYY/jMM/jDD',
+    monthYearA11yLabel: 'jYYYY jMMMM',
+  },
+};
 
 @Component({
   selector: 'app-date-picker',
@@ -32,9 +42,9 @@ export const PERSIAN_DATE_FORMATS = { parse: { dateInput: 'YYYY/MM/DD' }, displa
 export class DatePicker {
 
 
-
+  control = new FormControl();
   label = input.required<string>()
-  control = new FormControl()
+
   valueChange = output<string>()
 
   @Input({required: false})
@@ -43,24 +53,27 @@ export class DatePicker {
     this.control.setValue(gDate)
   }
 
+  @Input({required: false}) validators: ValidatorFn[] = []
   @Input({required: false}) initValue: boolean = false
 
-
   ngOnInit(): void {
-    this.control.valueChanges.subscribe(date =>{
+    if (this.validators && this.validators.length) {
+      this.control.setValidators(this.validators);
+      this.control.updateValueAndValidity(); // Important to recalc validity
+    }
+
+
+    this.control.valueChanges.subscribe((date: any) =>{
       if (!date) {
         this.valueChange.emit('');
         return;
       }
       const jDate = moment(date).locale('fa').format('jYYYY/jMM/jDD');
       this.valueChange.emit(jDate);
-
     })
 
-
-
     if(this.initValue === true) {
-      this.control.setValue(new globalThis.Date())
+      this.control.setValue(moment())
     }
   }
 
